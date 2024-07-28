365abdd55ff2eff99a9254442918cc89b18fb450
czyber <czypka.github@gmail.com>
2024-07-28 16:13:45 +0200

    Add mypy

--- codr/storage/mapper/base.py
+++ codr/storage/mapper/base.py
@@ -1,15 +1,16 @@
 from abc import ABC, abstractmethod
 
-from codr.storage.utils import E, M
+from codr.application.entities import Entity
+from codr.models import Base
 
 
 class Mapper(ABC):
     @staticmethod
     @abstractmethod
-    def to_entity(model: M) -> E:
+    def to_entity(model: Base) -> Entity:
         raise NotImplementedError
 
     @staticmethod
     @abstractmethod
-    def to_model(entity: E) -> M:
+    def to_model(entity: Entity) -> Base:
         raise NotImplementedError
--- codr/utils.py
+++ codr/utils.py
@@ -12,6 +12,13 @@ Id = str
 IdType = TypeVar("IdType", bound=Id)
 
 
+def get_env_var(name: str) -> str:
+    env_var = os.getenv(name)
+    if env_var is None:
+        raise ValueError(f"Environment variable {name} not set")
+    return env_var
+
+
 @dataclass
 class GitHubCredentials:
     client_id: str
@@ -19,9 +26,11 @@ class GitHubCredentials:
 
     @classmethod
     def load(cls) -> "GitHubCredentials":
+        client_id = get_env_var("GITHUB_CLIENT_ID")
+        client_secret = get_env_var("GITHUB_CLIENT_SECRET")
         return cls(
-            client_id=os.getenv("GITHUB_CLIENT_ID"),
-            client_secret=os.getenv("GITHUB_CLIENT_SECRET"),
+            client_id=client_id,
+            client_secret=client_secret,
         )
 
 
@@ -31,4 +40,5 @@ class RedirectUri:
 
     @classmethod
     def load(cls) -> "RedirectUri":
-        return cls(uri=os.getenv("REDIRECT_URI"))
+        uri = get_env_var("REDIRECT_URI")
+        return cls(uri=uri)
